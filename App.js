7eb0558c3d9a873c32ea3d8a163f71c687011fd8
Ajay-ee13 <ajaykumar.ee13@gmail.com>
2023-01-10 21:01:01 +0530

    Folder created and assests added

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Text, View, Alert, ActivityIndicator } from "react-native";
+import Styles from "./App.styles";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+const App = () => {
+  return(
+    <View style={Styles.root}>
+      <Text style={Styles.title} >Which of these is the glass</Text>
     </View>
-  );
-}
+  )
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+
+export default App;
